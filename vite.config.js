d8dba62906b4e241a2675bed48a73d8524262cf0
karamat ghous <ghouskaramat46@gmail.com>
2025-09-14 22:51:27 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// NOTE: If deploying to GitHub Pages at https://<user>.github.io/<repo>/
-// set base to '/<repo>/' below. For localhost, '' is fine.
 export default defineConfig({
   plugins: [react()],
-  base: '' // change to '/chakswari-hospital/' for GitHub Pages
+  base: '/CharityWebsite/' // must match your repo name
 })
